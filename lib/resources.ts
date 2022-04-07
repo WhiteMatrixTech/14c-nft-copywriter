52013ad3a3bebe253557be865651f7da99f42bee
feibowei <1906524955@qq.com>
2022-04-07 10:47:45 +0800

    feat: update copywriter

---
+++ lib/resources.ts
@@ -0,0 +1,16 @@
+import global from './global';
+import cosmos from './cosmos';
+import cocoon from './cocoon';
+import home from './home';
+import map from './map';
+import paper from './paper';
+
+
+export const resources = {
+  ...global,
+  ...cocoon,
+  ...cosmos,
+  ...home,
+  ...map,
+  ...paper,
+}
